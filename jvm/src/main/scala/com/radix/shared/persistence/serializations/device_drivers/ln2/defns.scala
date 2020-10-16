5991491337e8735e2f8fab43322b999579bc7e36
DhashS <dhash@outlook.com>
2020-10-16 01:48:15 +0000

    Merge branch 'vm/ln2-intergration-testing' into 'master'
    Vm/ln2 intergration testing
    
    See merge request radix-labs/monorepo!275
    
    GitOrigin-RevId: fbd9cb30d2c08795e5c111106ac49febad88f36e

--- jvm/src/main/scala/com/radix/shared/persistence/serializations/device_drivers/ln2/defns.scala
+++ jvm/src/main/scala/com/radix/shared/persistence/serializations/device_drivers/ln2/defns.scala
@@ -1,5 +1,18 @@
 package com.radix.shared.persistence.serializations.device_drivers.ln2
 
+import akka.actor.ExtendedActorSystem
 import akka.actor.typed.ActorRef
+import com.radix.shared.persistence.ActorRefSerializer._
+import com.radix.shared.persistence.AvroSerializer
 
-object defns {}
+object defns {
+
+  sealed trait LNLevel
+  final case class LNLevelLow() extends LNLevel
+  final case class LNLevelNormal() extends LNLevel
+  final case class LNLevelRequest(replyTo: ActorRef[LNLevel])
+
+  class LNLevelLowSerializer extends AvroSerializer[LNLevelLow]
+  class LNLevelNormalSerializer extends AvroSerializer[LNLevelNormal]
+  class LNLevelRequestSerializer(implicit eas: ExtendedActorSystem) extends AvroSerializer[LNLevelRequest]
+}
