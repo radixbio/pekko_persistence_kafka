737a7c41d08d510a08c194eee4bc9edeafb70fa8
Vadym Matviichuk <vadym.matviichuk15@gmail.com>
2022-11-07 16:17:42 -0500

    Frontdoor Avro Implementation (#643)
    * start work on replacing the frontdoor
    
    * delete old frontdoor stuff and drafts of the new system
    
    * api looks decent, start drafting the http server
    
    * non cluster version of frontdoor works
    
    * wip
    
    * rolling into a newer model of frontdoor
    
    * wip deps
    
    * wip deps
    
    * wip
    
    * most of the functionality ready, need to add docs and split into files
    
    * added kill on stream end in subscription and starting to work on docs
    
    * Lots of comments, README left
    
    * Clean up, fixed readme, added export targets
    
    * fixes after merge
    
    * couple more logs
    
    * some fixes
    
    * fix monolith
    
    * clean up, a lot more docs, deleted old files, changed a dependency version
    
    * fix derivation due to some library version difference, should be good to go now
    
    Co-authored-by: Oleks Litus <oleks.litus@gmail.com>
    GitOrigin-RevId: 5167360b0bdb2a41cdfabdcfc76e71917ae739f2

--- src/main/scala/AvroSerializer.scala
+++ src/main/scala/AvroSerializer.scala
@@ -11,6 +11,7 @@ class AvroSerializer[T: SchemaFor: ClassTag: Decoder: Encoder]()(implicit ev: T 
     extends SerializerWithStringManifest {
   val Manifest = implicitly[ClassTag[T]].toString
   val schema: SchemaFor[T] = implicitly
+  val classTag = implicitly[ClassTag[T]]
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
     case Manifest => {
@@ -29,9 +30,9 @@ class AvroSerializer[T: SchemaFor: ClassTag: Decoder: Encoder]()(implicit ev: T 
   override def toBinary(o: AnyRef): Array[Byte] = {
     val baos = new ByteArrayOutputStream()
     o match {
-      case mf: T =>
+      case classTag(obj) =>
         val os = AvroOutputStream.data[T].to(baos).build()
-        os.write(mf)
+        os.write(obj)
         os.flush()
         os.close()
       case els =>
@@ -48,4 +49,57 @@ class AvroSerializer[T: SchemaFor: ClassTag: Decoder: Encoder]()(implicit ev: T 
   override def manifest(o: AnyRef): String = o.getClass.getName
 
   override def identifier: Int = implicitly[ClassTag[T]].toString.hashCode()
+
+  /**
+   * Use Avro to serialize object `o` to an array of bytes.
+   * As of now, to/fromBinary use data, which embeds entire schema in each message.
+   * First, this is not supported by frontend so we need to use binary which just has data.
+   * Second, this is way faster which is good.
+   * For examples, look at [[com.radix.frontdoor.gen]]
+   */
+  def toBinaryFrontend(o: AnyRef): Array[Byte] = {
+    val baos = new ByteArrayOutputStream()
+    o match {
+      case classTag(obj) =>
+        val os = AvroOutputStream.binary[T].to(baos).build()
+        os.write(obj)
+        os.flush()
+        os.close()
+      case els =>
+        throw new NotSerializableException(
+          s"this serializer can only serialize $Manifest, you gave $els, of type ${els.getClass.getName}"
+        )
+    }
+    baos.flush()
+    val res = baos.toByteArray
+    baos.close()
+    res
+  }
+
+  /**
+   * Use Avro to deserialize an array of bytes into an object with `manifest`.
+   * Manifest for us is a class path.
+   * As of now, to/fromBinary use data, which embeds entire schema in each message.
+   * First, this is not supported by frontend so we need to use binary which just has data.
+   * Second, this is way faster which is good.
+   * For examples, look at [[com.radix.frontdoor.gen]]
+   *
+   * @param bytes byte array to deserialize
+   * @param manifest manifest of the target to deserialize
+   * @return deserialized object
+   */
+  def fromBinaryFrontend(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
+
+    case Manifest => {
+      val is = AvroInputStream.binary[T].from(bytes).build(AvroSchema[T])
+      val res: T = is.iterator.next()
+      is.close()
+      ev.apply(res)
+    }
+
+    case els =>
+      throw new NotSerializableException(
+        s"this serializer only can deserialize for $Manifest, you gave $els, of type ${els.getClass.getName}, with manifest $manifest"
+      )
+  }
 }
