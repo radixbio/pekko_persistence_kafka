40a013556d0c6f8b4ca8118efdf6cfcf9edb2cde
Vadym Matviichuk <vadym.matviichuk15@gmail.com>
2023-05-05 16:28:10 -0400

    Add Json 'serialization' to frontdoor as a dev option (#1129)
    * wip
    
    * json api prototype ready
    
    * comments and docs
    
    * fmt
    
    * make nicer json-switching
    
    * fix logic
    
    * fix logic
    
    * switch serialization to look nicer and make more sense
    
    * some more work on nicer json serialization
    
    * comments + fix type signatures
    
    * fix build
    
    * fix tests
    
    ---------
    
    Co-authored-by: Vadym Matviichuk <vadym@radix.bio>
    Co-authored-by: Dhash Shrivathsa <me@dha.sh>
    GitOrigin-RevId: d51a09efb3e4f0cd2a7aee55f1a5188cc7632c7b

--- src/main/scala/AvroSerializer.scala
+++ src/main/scala/AvroSerializer.scala
@@ -15,9 +15,29 @@ class AvroSerializer[T: SchemaFor: ClassTag: Decoder: Encoder]()(implicit ev: T 
   val encoder: Encoder[T] = implicitly
   val decoder: Decoder[T] = implicitly
 
-  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
+  /**
+   * Deserialize the array of bytes into an object of type [[T]]. Uses Avro binary format to deserialize.
+   * Used by Akka serialization.
+   * @param bytes bytes to deserialize
+   * @param manifest manifest
+   * @return deserialized object
+   */
+  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef =
+    fromBinary(bytes, manifest, useJson = false)
+
+  /**
+   * Deserialize the array of bytes into an object of type [[T]].
+   * @param bytes bytes to deserialize
+   * @param manifest manifest for the array of bytes to verify the validity of this operation
+   * @param useJson true - treat bytes as a UTF-8 encode JSON string
+   *                false - treat bytes as an Avro encoded binary string
+   * @return deserialized object
+   */
+  def fromBinary(bytes: Array[Byte], manifest: String, useJson: Boolean): AnyRef = manifest match {
     case Manifest => {
-      val is = AvroInputStream.binary[T].from(bytes).build(AvroSchema[T])
+      val is =
+        if (useJson) AvroInputStream.json[T].from(bytes).build(AvroSchema[T])
+        else AvroInputStream.binary[T].from(bytes).build(AvroSchema[T])
       val res: T = is.iterator.next()
       is.close()
       ev.apply(res)
@@ -29,11 +49,28 @@ class AvroSerializer[T: SchemaFor: ClassTag: Decoder: Encoder]()(implicit ev: T 
       )
   }
 
-  override def toBinary(o: AnyRef): Array[Byte] = {
+  /**
+   * Serialize object to array of bytes using avro binary format.
+   * Used by Akka serialization.
+   * @param o object to serialize
+   * @return array of bytes
+   */
+  override def toBinary(o: AnyRef): Array[Byte] = toBinary(o, useJson = false)
+
+  /**
+   * Serialize given object into an array of bytes.
+   * @param o object to serialize
+   * @param useJson true - serialize to a bytes representing an UTF-8 encoded JSON string,
+   *                false - serialize to avro encoded binary string
+   * @return array of bytes
+   */
+  def toBinary(o: AnyRef, useJson: Boolean): Array[Byte] = {
     val baos = new ByteArrayOutputStream()
     o match {
       case classTag(obj) =>
-        val os = AvroOutputStream.binary[T].to(baos).build()
+        val os =
+          if (useJson) AvroOutputStream.json[T].to(baos).build()
+          else AvroOutputStream.binary[T].to(baos).build()
         os.write(obj)
         os.flush()
         os.close()
